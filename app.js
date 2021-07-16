a370956eca6d1b44628454e69f4148cddef5e26f
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-07-16 17:41:34 +0900

    project start

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// hello
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const connect = require("./schemas");
+connect();
+
+//checking mongoose db connection
+// const mongoose = require("mongoose");
+// app.get("/mongodb", async (req, res) => {
+//     await mongoose.connect("mongodb://localhost/clone", {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: true,
+//         useCreateIndex: true,
+//     });
+
+
+//     const { Schema } = mongoose;
+//     const blogsSchema = new Schema({
+//         postId: {
+//             type: Number,
+//             required: true,
+//             unique: true,
+//         },
+//     });
+
+//     let blogs = mongoose.model("blogs", blogsSchema);
+//     await blogs.create({
+//         postId: 1,
+//     });
+
+
+// res.send("ok");
+// });
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ schemas/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+    mongoose
+        .connect("mongodb://localhost/clone:27017", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            ignoreUndefined: true,
+        })
+        .catch((err) => console.log(err));
+};
+
+
+
+
+
+
+mongoose.connection.on("error", (err) => {
+    console.error("몽고디비 연결 에러", err);
+});
+
+module.exports = connect;
